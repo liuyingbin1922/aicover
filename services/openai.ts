067fb55ad9969fa35d81d69f9a39c1648613120e
yingbin <yingbin@yingbindeMBP.lan>
2025-01-01 10:30:08 +0800

    feat: baseURL

--- services/openai.ts
+++ services/openai.ts
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 export function getOpenAIClient() {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-    baseUrl: 'https://api.zhizengzeng.com/v1',
+    baseURL: 'https://api.zhizengzeng.com/v1',
   });
 
   return openai;
