ce356efe054bfa843ab6e4737f2590f44764d1cf
yingbin <yingbin@yingbindeMBP.lan>
2025-01-01 10:24:53 +0800

    feat: 补充环境变量

--- models/db.ts
+++ models/db.ts
@@ -8,7 +8,12 @@ export function getDb() {
     console.log("connectionString", connectionString);
 
     globalPool = new Pool({
-      connectionString,
+      // connectionString,
+      // user: 'postgres', // 数据库用户名
+      // host: 'your-database-host.supabase.co', // Supabase 数据库的 URL
+      // database: 'postgres', // 数据库名称
+      // password: 'your-database-password', // 数据库密码
+      // port: 5432, // 默认 PostgreSQL 端口
     });
   }
 
